52f8839acc88d307738823631369b83a616ee922
banifach1228-prog <banifach1228@gmail.com>
2026-09-13 00:47:00 +0300

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -5,7 +5,7 @@ import miniapp_server
 
 
 def main():
-    # База загружается один раз ДО запуска Mini App и Telegram-бота.
+    # Загружаем базу один раз до запуска обоих компонентов.
     bot.load_db()
 
     print("BulbaMaxAI: database loaded")
@@ -18,13 +18,8 @@ def main():
 
     web_thread.start()
 
-    print(
-        "BulbaMaxAI: Mini App server started"
-    )
-
-    print(
-        "BulbaMaxAI: Telegram bot starting"
-    )
+    print("BulbaMaxAI: Mini App server started")
+    print("BulbaMaxAI: Telegram bot starting")
 
     bot.main()
 
